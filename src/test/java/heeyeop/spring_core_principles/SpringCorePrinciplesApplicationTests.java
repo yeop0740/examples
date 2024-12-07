23aa531add04cf18495d965b4e42bf84b19d6951
heeyeop Kim <ssucgoo@gmail.com>
2024-12-07 23:01:35 +0900

    :tada: 프로젝트 생성

---
+++ src/test/java/heeyeop/spring_core_principles/SpringCorePrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package heeyeop.spring_core_principles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrinciplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
