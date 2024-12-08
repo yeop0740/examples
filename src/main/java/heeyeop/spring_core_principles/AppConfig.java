a1ae1e7a74ec1f6690cc41f730e3deaa0ed53ade
heeyeop Kim <ssucgoo@gmail.com>
2024-12-08 17:28:14 +0900

    :necktie: appconfig를 이용한 할인 정책 변경

--- src/main/java/heeyeop/spring_core_principles/AppConfig.java
+++ src/main/java/heeyeop/spring_core_principles/AppConfig.java
@@ -1,7 +1,7 @@
 package heeyeop.spring_core_principles;
 
 import heeyeop.spring_core_principles.discount.DiscountPolicy;
-import heeyeop.spring_core_principles.discount.FixDiscountPolicy;
+import heeyeop.spring_core_principles.discount.RateDiscountPolicy;
 import heeyeop.spring_core_principles.member.MemberRepository;
 import heeyeop.spring_core_principles.member.MemberService;
 import heeyeop.spring_core_principles.member.MemberServiceImpl;
@@ -24,6 +24,6 @@ public class AppConfig {
     }
 
     private static DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
