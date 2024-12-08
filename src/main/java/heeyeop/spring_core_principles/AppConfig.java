912dd415dc066914a8255e905ef9efb219ad3c65
heeyeop Kim <ssucgoo@gmail.com>
2024-12-08 17:19:09 +0900

    :recycle: appconfig 파일 중복 부분 제거

--- src/main/java/heeyeop/spring_core_principles/AppConfig.java
+++ src/main/java/heeyeop/spring_core_principles/AppConfig.java
@@ -1,6 +1,8 @@
 package heeyeop.spring_core_principles;
 
+import heeyeop.spring_core_principles.discount.DiscountPolicy;
 import heeyeop.spring_core_principles.discount.FixDiscountPolicy;
+import heeyeop.spring_core_principles.member.MemberRepository;
 import heeyeop.spring_core_principles.member.MemberService;
 import heeyeop.spring_core_principles.member.MemberServiceImpl;
 import heeyeop.spring_core_principles.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import heeyeop.spring_core_principles.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
