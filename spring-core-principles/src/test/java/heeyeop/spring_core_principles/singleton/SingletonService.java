94a23082cbff304302a127c2d3544866f401684c
heeyeop Kim <ssucgoo@gmail.com>
2024-12-16 13:51:10 +0900

    :white_check_mark: 싱글톤 패턴

---
+++ spring-core-principles/src/test/java/heeyeop/spring_core_principles/singleton/SingletonService.java
@@ -0,0 +1,16 @@
+package heeyeop.spring_core_principles.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService INSTANCE = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return INSTANCE;
+    }
+
+    private SingletonService() {}
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
--- spring-core-principles/src/test/java/heeyeop/spring_core_principles/singleton/SingletonTest.java
+++ spring-core-principles/src/test/java/heeyeop/spring_core_principles/singleton/SingletonTest.java
@@ -2,9 +2,10 @@ package heeyeop.spring_core_principles.singleton;
 
 import heeyeop.spring_core_principles.AppConfig;
 import heeyeop.spring_core_principles.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SingletonTest {
 
@@ -21,6 +22,19 @@ public class SingletonTest {
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+//        new SingletonService(); // 'SingletonService()' has private access in 'heeyeop. spring_core_principles. singleton. SingletonService'
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
