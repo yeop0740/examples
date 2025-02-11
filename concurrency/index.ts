0d23d9329cdf0baf028f428218bc7b9d8d479189
heeyeop Kim <ssucgoo@gmail.com>
2025-02-11 23:35:25 +0900

    :bug: request body 등을 파싱할 수 있도록 미들웨어 설정

--- concurrency/index.ts
+++ concurrency/index.ts
@@ -6,6 +6,9 @@ import { prisma } from "./init";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (_, res) => {
   res.send({ hello: "world" });
 });
