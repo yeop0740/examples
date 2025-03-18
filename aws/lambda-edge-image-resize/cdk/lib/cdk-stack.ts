f3efb84f5533dbda46fac85cd01bb92ba995fe55
heeyeop <ssucgoo@gmail.com>
2025-03-18 22:17:23 +0900

    :sparkles: cloudfront, distribution 의 origin access control 생성

--- aws/lambda-edge-image-resize/cdk/lib/cdk-stack.ts
+++ aws/lambda-edge-image-resize/cdk/lib/cdk-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {BlockPublicAccess, Bucket} from 'aws-cdk-lib/aws-s3';
+import {
+    Distribution,
+    ViewerProtocolPolicy
+} from 'aws-cdk-lib/aws-cloudfront';
+import {S3BucketOrigin} from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class CdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +14,13 @@ export class CdkStack extends cdk.Stack {
         const imageBucket = new Bucket(this, 'LambdaEdgeImageResizeBucket', {
             publicReadAccess: false,
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL
+        });
+
+        const distribution = new Distribution(this, 'LambdaEdgeImageResizeDistribution', {
+            defaultBehavior: {
+                origin: S3BucketOrigin.withOriginAccessControl(imageBucket),
+                viewerProtocolPolicy: ViewerProtocolPolicy.ALLOW_ALL,
+            },
         });
     }
 }
