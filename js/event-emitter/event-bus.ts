10a25aeefcacf4307ab69af5ee06f1ebc8066dd0
heeyeop <ssucgoo@gmail.com>
2025-07-09 22:57:56 +0900

    :sparkles: 이벤트 핸들러에 슬로우 쿼리 추가

--- js/event-emitter/event-bus.ts
+++ js/event-emitter/event-bus.ts
@@ -1,7 +1,10 @@
+import { PrismaClient } from './generated/prisma'
 import { EventEmitter } from 'node:events'
 
 export const eventEmitter = new EventEmitter()
+const prisma = new PrismaClient()
 
-eventEmitter.on('POST_CREATED', (data) => {
+eventEmitter.on('POST_CREATED', async (data) => {
 	console.log('POST_CREATED', data)
+	await prisma.$queryRaw`SELECT pg_sleep(1)`
 })
