aa8123958d9352e29225bf72aaddaeb8bfc98ac4
heeyeop <ssucgoo@gmail.com>
2025-07-10 00:05:01 +0900

    :adhesive_bandage: 쿼리 실행 로그 추가 및 타입 변경을 위한 api 변경

--- js/event-emitter/event-bus.ts
+++ js/event-emitter/event-bus.ts
@@ -2,9 +2,11 @@ import { PrismaClient } from './generated/prisma'
 import { EventEmitter } from 'node:events'
 
 export const eventEmitter = new EventEmitter()
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+	log: ['query']
+})
 
 eventEmitter.on('POST_CREATED', async (data) => {
 	console.log('POST_CREATED', data)
-	await prisma.$queryRaw`SELECT pg_sleep(1)`
+	await prisma.$executeRaw`SELECT pg_sleep(1)`
 })
