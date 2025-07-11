2b309c45781c3ab0dcebf05bf3f4c07f4113b185
heeyeop <ssucgoo@gmail.com>
2025-07-11 22:18:44 +0900

    :white_check_mark: supertest 이용한 테스트 추가

---
+++ js/event-emitter/event-supertest.test.ts
@@ -0,0 +1,27 @@
+import { eventEmitter } from './event-bus'
+import { app } from './server'
+import request from 'supertest'
+
+describe('GET "/" 요청에 대해서', () => {
+	it('Hello World 문자열을 리턴한다', async () => {
+		const response = await request(app).get('/')
+
+		expect(response.status).toEqual(200)
+		expect(response.type).toEqual('text/html')
+		expect(response.text).toEqual('Hello World!')
+	})
+})
+
+describe('POST "/event" 요청에 대해서', () => {
+	beforeAll(() => {
+		eventEmitter
+	})
+
+	it('POST_CREATED 이벤트를 발급 하고 Event emitted 문자열을 리턴한다', async () => {
+		const response = await request(app).post('/event')
+        
+		expect(response.status).toEqual(200)
+		expect(response.type).toEqual('text/html')
+		expect(response.text).toEqual('Event emitted')
+	})
+})
