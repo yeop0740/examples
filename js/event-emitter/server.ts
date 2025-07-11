0eff400fb66d74139429bd112150d62c9da9f3de
heeyeop <ssucgoo@gmail.com>
2025-07-11 22:16:45 +0900

    :building_construction: 서버 앱 설정과 실행 부분 분리

--- js/event-emitter/server.ts
+++ js/event-emitter/server.ts
@@ -1,9 +1,8 @@
-import express, { type Request, type Response } from 'express'
+import express, { Request, Response } from 'express'
 import { eventEmitter } from './event-bus'
 // import { Client } from 'pg'
 
-const app = express()
-const port = 3001
+export const app = express()
 
 // const client = new Client()
 // await client.connect()
@@ -12,36 +11,10 @@ const port = 3001
 // console.log(res.rows[0].message) // Hello world!
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Hello World!')
+	res.status(200).send('Hello World!')
 })
 
 app.post('/event', (req: Request, res: Response) => {
 	eventEmitter.emit('POST_CREATED', 'Hello World!')
 	res.send('Event emitted')
 })
-
-const server = app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
-
-const gracefulShutdown = () => {
-	console.log('Received shutdown signal, shutting down gracefully...')
-	server.close(async () => {
-		console.log('Closed out remaining connections.')
-		// 여기에 데이터베이스 연결 종료 등 리소스 정리 코드를 추가할 수 있습니다.
-		// await client.end()
-		process.exit(0)
-	})
-
-	// 강제 종료 타이머
-	setTimeout(() => {
-		console.error(
-			'Could not close connections in time, forcefully shutting down',
-		)
-		process.exit(1)
-	}, 10000) // 10초
-}
-
-// 앱 종료 시그널 리스닝
-process.on('SIGTERM', gracefulShutdown)
-process.on('SIGINT', gracefulShutdown)
