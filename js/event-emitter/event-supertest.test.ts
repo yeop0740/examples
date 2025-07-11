4cd86385fa51a70cddae0e4c93921efc435558e8
heeyeop <ssucgoo@gmail.com>
2025-07-11 23:11:38 +0900

    :white_check_mark: supertest 를 사용한 테스트 스킵설정

--- js/event-emitter/event-supertest.test.ts
+++ js/event-emitter/event-supertest.test.ts
@@ -2,7 +2,7 @@ import { eventEmitter } from './event-bus'
 import { app } from './server'
 import request from 'supertest'
 
-describe('GET "/" 요청에 대해서', () => {
+describe.skip('GET "/" 요청에 대해서', () => {
 	it('Hello World 문자열을 리턴한다', async () => {
 		const response = await request(app).get('/')
 
@@ -12,14 +12,14 @@ describe('GET "/" 요청에 대해서', () => {
 	})
 })
 
-describe('POST "/event" 요청에 대해서', () => {
+describe.skip('POST "/event" 요청에 대해서', () => {
 	beforeAll(() => {
 		eventEmitter
 	})
 
 	it('POST_CREATED 이벤트를 발급 하고 Event emitted 문자열을 리턴한다', async () => {
 		const response = await request(app).post('/event')
-        
+
 		expect(response.status).toEqual(200)
 		expect(response.type).toEqual('text/html')
 		expect(response.text).toEqual('Event emitted')
