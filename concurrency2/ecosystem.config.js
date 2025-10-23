3dddf666923e8f84fd6d31c312a02170f95896da
heeyeop <ssucgoo@gmail.com>
2025-10-23 22:42:26 +0900

    :wrench: pm2 env 변경

--- concurrency2/ecosystem.config.js
+++ concurrency2/ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       name: 'concurrency2-app',
       script: 'dist/main.js',
-      instances: 'max',
+      instances: 4,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        DATABASE_URL: 'postgres://postgres:1234@localhost:5453/postgres?schema=public', // 실제 DB URL로 변경
+        DATABASE_URL: 'postgres://postgres:1234@localhost:5453/postgres?schema=public&connection_limit=5', // 실제 DB URL로 변경
         JWT_SECRET: 'secret', // 실제 JWT SECRET으로 변경
       },
     },
