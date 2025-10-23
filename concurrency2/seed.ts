f3ddb069d94605d8d6e868af72de513c962f2002
heeyeop <ssucgoo@gmail.com>
2025-10-23 22:41:22 +0900

    :hammer: 데이터 생성 및 삭제 스크립트 작성

---
+++ concurrency2/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function seeding() {
+    const user = await prisma.user.create({
+        data: {
+            id: 'c2e36bd6-7424-4487-a82e-991c4a997231',
+            email: 'test@gmail.com',
+            password: '$2b$10$5d2DoICXTV55374PkhvfZ.dh15UFzLf3GsLmaLv54YxVNzRrigMt2',
+        }
+    });
+}
+
+seeding()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    });
