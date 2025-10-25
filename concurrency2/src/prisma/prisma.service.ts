eeffcf71efdcf556623b933ab5e3e840f0f2ead7
heeyeop <ssucgoo@gmail.com>
2025-10-25 16:27:42 +0900

    :technologist: 디버깅 용이하도록 설정 및 로직 변경
    - prisma 에서 전달하는 로그를 출력하도록 변경하였습니다.
    - 인증 과정에서 jwt 토큰의 만료를 확인하지 않도록 변경하였습니다.

--- concurrency2/src/prisma/prisma.service.ts
+++ concurrency2/src/prisma/prisma.service.ts
@@ -1,9 +1,44 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger: Logger;
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
+      errorFormat: 'pretty',
+    });
+    this.logger = new Logger(PrismaService.name);
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
   async onModuleInit() {
+    this.log();
     await this.$connect();
+  }
+
+  log() {
+    this.$on('query', (e) => this.logger.log(`[query]: ${e.query}`));
+    this.$on('info', (e) => this.logger.log(e.message));
+    this.$on('warn', (e) => this.logger.log(e));
+    this.$on('error', (e) => this.logger.log(e));
   }
 }
