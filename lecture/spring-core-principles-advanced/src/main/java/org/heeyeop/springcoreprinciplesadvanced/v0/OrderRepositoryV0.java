3292e3b0ae935e0bbb384e1562112c4963217de0
heeyeop <ssucgoo@gmail.com>
2026-01-01 20:58:28 +0900

    :sparkles: 기본 기능 생성

---
+++ lecture/spring-core-principles-advanced/src/main/java/org/heeyeop/springcoreprinciplesadvanced/v0/OrderRepositoryV0.java
@@ -0,0 +1,27 @@
+package org.heeyeop.springcoreprinciplesadvanced.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        // 저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        // 영속성 관련 코드
+        sleep(1000);
+    }
+
+    private void sleep(int millisecond) {
+        try {
+            Thread.sleep(millisecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
