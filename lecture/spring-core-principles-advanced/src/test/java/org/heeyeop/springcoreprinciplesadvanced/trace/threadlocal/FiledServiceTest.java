786c67c24ed6dd2c5fe47f2554aac606de0804ea
heeyeop <ssucgoo@gmail.com>
2026-01-04 20:20:44 +0900

    :construction: 동시성 문제 예제 코드 추가

---
+++ lecture/spring-core-principles-advanced/src/test/java/org/heeyeop/springcoreprinciplesadvanced/trace/threadlocal/FiledServiceTest.java
@@ -0,0 +1,62 @@
+package org.heeyeop.springcoreprinciplesadvanced.trace.threadlocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.heeyeop.springcoreprinciplesadvanced.trace.threadlocal.code.FieldService;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FiledServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field_synchronous() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("tread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("tread-B");
+
+        threadA.start();
+        sleep(2000); // A 의 실행 대기
+        threadB.start();
+        sleep(2000); // B 의 실행 대기
+        log.info("main end");
+    }
+
+    @Test
+    void field_asynchronous() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("tread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("tread-B");
+
+        threadA.start();
+        threadB.start();
+        sleep(3000); // B 의 실행 대기
+        log.info("main end");
+    }
+
+    void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
