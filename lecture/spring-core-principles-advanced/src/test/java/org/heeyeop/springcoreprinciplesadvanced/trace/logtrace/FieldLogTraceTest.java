c4795cf511f64b9d9a95d8922b0736b036622830
heeyeop <ssucgoo@gmail.com>
2026-01-04 17:31:51 +0900

    :white_check_mark: 필드를 이용한 로그 context 유지 기능 구현 확인을 위한 출력 코드 추가

---
+++ lecture/spring-core-principles-advanced/src/test/java/org/heeyeop/springcoreprinciplesadvanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,27 @@
+package org.heeyeop.springcoreprinciplesadvanced.trace.logtrace;
+
+import org.heeyeop.springcoreprinciplesadvanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        Exception e = new IllegalStateException();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2, e);
+        trace.exception(status1, e);
+    }
+
+}
