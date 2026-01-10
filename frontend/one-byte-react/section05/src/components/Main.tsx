19093bf67dd5e1d80ba78d5569d6389c6ec4af51
heeyeop <ssucgoo@gmail.com>
2026-01-10 15:45:42 +0900

    :sparkles: 컴포넌트 사용 방법 예제 코드 추가

--- frontend/one-byte-react/section05/src/components/Main.tsx
+++ frontend/one-byte-react/section05/src/components/Main.tsx
@@ -1,7 +1,13 @@
 const Main = () => {
+    const number = 10;
+
     return (
         <main>
             <h1>main</h1>
+            <h2>{number % 2 === 0 ? '짝수' : '홀수'}</h2>
+            {10}
+            {number}
+            {[1, 2, 3]}
         </main>
     );
 };
