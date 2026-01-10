b5604d4f5928f31563d33bc952e4e842d89ab587
heeyeop <ssucgoo@gmail.com>
2026-01-10 15:23:59 +0900

    :sparkles: 헤더 컴포넌트 작성

--- frontend/one-byte-react/section05/src/App.tsx
+++ frontend/one-byte-react/section05/src/App.tsx
@@ -1,5 +1,18 @@
 import './App.css'
 
+/**
+ * 함수 컴포넌트
+ * 컴포넌트는 아래와 같은 함수 컴포넌트, 화살표 함수 컴포넌트, 클래스 컴포턴트 형태로 작성할 수 있다.
+ * 컴포넌트는 첫 글자를 대문자로 작성해야 한다.
+ */
+function Header() {
+    return (
+        <header>
+            <h1>header</h1>
+        </header>
+    );
+}
+
 function App() {
   return (
     <>
