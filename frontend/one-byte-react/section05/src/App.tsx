e5414072c52d6c8cb5e057563860828aa624554a
heeyeop <ssucgoo@gmail.com>
2026-01-10 16:48:46 +0900

    :sparkles: state 값, 함수 예제 코드 추가

--- frontend/one-byte-react/section05/src/App.tsx
+++ frontend/one-byte-react/section05/src/App.tsx
@@ -1,28 +1,25 @@
-import './App.css'
-import Button from "./components/Button.tsx";
+import {useState} from "react";
+import './App.css';
 
 /**
  * Header 컴포넌트는 App 컴포넌트의 자식 컴포넌트가 된다.
  * App 컴포넌트는 Header 컴포넌트의 부모 컴포넌트가 된다.
  */
 function App() {
-    const buttonProps = {
-        text: "메일",
-        color: "red",
-        a: 1,
-        b: 2,
-        c: 3,
-    };
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-        <Button {...buttonProps} />
-        <Button text={"카페"} color={"blue"} />
-        <Button text={"블로그"}>
-            <div>자식 요소</div>
-        </Button>
-    </>
-  )
+    return (
+        <>
+            {count}
+            <button
+                onClick={() => {
+                    setCount(count + 1);
+                }}
+            >
+                +
+            </button>
+        </>
+    );
 }
 
 export default App
