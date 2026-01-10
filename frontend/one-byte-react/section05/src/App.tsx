f670f2ef1d06ce88eaed2bc938b5822e62c719f7
heeyeop <ssucgoo@gmail.com>
2026-01-10 15:30:51 +0900

    :truck: 컴포넌트 분리

--- frontend/one-byte-react/section05/src/App.tsx
+++ frontend/one-byte-react/section05/src/App.tsx
@@ -1,17 +1,5 @@
 import './App.css'
-
-/**
- * 함수 컴포넌트
- * 컴포넌트는 아래와 같은 함수 컴포넌트, 화살표 함수 컴포넌트, 클래스 컴포턴트 형태로 작성할 수 있다.
- * 컴포넌트는 첫 글자를 대문자로 작성해야 한다.
- */
-function Header() {
-    return (
-        <header>
-            <h1>header</h1>
-        </header>
-    );
-}
+import Header from "./components/Header.tsx";
 
 /**
  * Header 컴포넌트는 App 컴포넌트의 자식 컴포넌트가 된다.
