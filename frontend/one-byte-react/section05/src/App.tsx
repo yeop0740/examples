90e85276f7e5382711b8594a49b1f25b5b1c9fe8
heeyeop <ssucgoo@gmail.com>
2026-01-10 17:20:20 +0900

    :sparkles: light 상태를 표현하기 위한 컴포넌트 추가

--- frontend/one-byte-react/section05/src/App.tsx
+++ frontend/one-byte-react/section05/src/App.tsx
@@ -1,6 +1,18 @@
 import {useState} from "react";
 import './App.css';
 
+const Bulb = ({light}) => {
+    console.log(light);
+
+    return (
+        <div>
+            {light === "ON" ?
+                <h1 style={{backgroundColor: "orange"}}>ON</h1> :
+                <h1 style={{backgroundColor: "gray"}}>OFF</h1>}
+        </div>
+    );
+}
+
 /**
  * Header 컴포넌트는 App 컴포넌트의 자식 컴포넌트가 된다.
  * App 컴포넌트는 Header 컴포넌트의 부모 컴포넌트가 된다.
@@ -12,7 +24,7 @@ function App() {
     return (
         <>
             <div>
-                <h1>{light}</h1>
+                <Bulb light={light} />
                 <button
                     onClick={() => {
                         setLight(light === "ON" ? "OFF" : "ON");
