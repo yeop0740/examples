74ee30ce64d80dc0d475edb6d1c9d6ef37c0634f
heeyeop <ssucgoo@gmail.com>
2026-01-10 16:54:15 +0900

    :sparkles: 하나의 컴포넌트에서 한 개 이상의 state 다루는 예제 코드 추가

--- frontend/one-byte-react/section05/src/App.tsx
+++ frontend/one-byte-react/section05/src/App.tsx
@@ -7,17 +7,29 @@ import './App.css';
  */
 function App() {
     const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF");
 
     return (
         <>
-            {count}
-            <button
-                onClick={() => {
-                    setCount(count + 1);
-                }}
-            >
-                +
-            </button>
+            <div>
+                <h1>{light}</h1>
+                <button
+                    onClick={() => {
+                        setLight(light === "ON" ? "OFF" : "ON");
+                    }}>
+                    {light === "ON" ? "끄기" : "켜기"}
+                </button>
+            </div>
+            <div>
+                {count}
+                <button
+                    onClick={() => {
+                        setCount(count + 1);
+                    }}
+                >
+                    +
+                </button>
+            </div>
         </>
     );
 }
