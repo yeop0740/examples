e206cd96262c03d24d1debd27b4b9989e313cfcd
heeyeop <ssucgoo@gmail.com>
2026-01-10 15:28:01 +0900

    :sparkles: 헤더 컴포넌트 렌더링 되도록 앱 컴포넌트에 추가

--- frontend/one-byte-react/section05/src/App.tsx
+++ frontend/one-byte-react/section05/src/App.tsx
@@ -13,9 +13,14 @@ function Header() {
     );
 }
 
+/**
+ * Header 컴포넌트는 App 컴포넌트의 자식 컴포넌트가 된다.
+ * App 컴포넌트는 Header 컴포넌트의 부모 컴포넌트가 된다.
+ */
 function App() {
   return (
     <>
+        <Header />
         <h1>안녕 리액트!</h1>
     </>
   )
