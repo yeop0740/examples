1e1a7eeb695c3bd9a1cef94132449d84cdd39395
heeyeop <ssucgoo@gmail.com>
2026-01-10 16:29:59 +0900

    :sparkles: 이벤트 핸들러 코드 추가(화살표 함수 사용하여 태그에 직접 추가)

--- frontend/one-byte-react/section05/src/components/Button.tsx
+++ frontend/one-byte-react/section05/src/components/Button.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 
 const Button = ({children, text, color = "black"}: {children?: React.ReactNode, text: string, color?: string}) => {
-    return <button style={{color: color}}>
+    return <button
+        onClick={() => console.log(text)}
+        style={{color: color}}>
         {text} - {color.toUpperCase()}
         {children}
     </button>;
