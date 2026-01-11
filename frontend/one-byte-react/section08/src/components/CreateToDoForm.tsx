94052f57b8ed901243086efaee20821773e7bf67
heeyeop <ssucgoo@gmail.com>
2026-01-11 16:34:01 +0900

    :lipstick: CreateTodoForm UI 설정

--- frontend/one-byte-react/section08/src/components/CreateToDoForm.tsx
+++ frontend/one-byte-react/section08/src/components/CreateToDoForm.tsx
@@ -1,9 +1,11 @@
+import "./CreateTodoForm.css";
+
 const CreateToDoForm = () => {
     return (
-        <div>
+        <section className="create-todo-form">
             <input placeholder="할 일을 입력하세요"/>
             <button>추가</button>
-        </div>
+        </section>
     )
 }
 
