45dcd950225038d02fcc8cb2f8f30f67969484f4
heeyeop <ssucgoo@gmail.com>
2026-01-13 21:43:46 +0900

    :sparkles: query string 사용 코드 추가

--- frontend/one-byte-react/section11/src/pages/Home.tsx
+++ frontend/one-byte-react/section11/src/pages/Home.tsx
@@ -1,4 +1,10 @@
+import {useSearchParams} from "react-router";
+
 const Home = () => {
+    const [params, setParams] = useSearchParams();
+    // domain.example?value=123 -> 123 출력
+    console.log(params.get("value"));
+
     return <div>Home</div>;
 }
 
