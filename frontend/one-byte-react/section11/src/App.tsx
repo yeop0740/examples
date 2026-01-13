ccb93836db0bc99a66186585d75620a29b6c278d
heeyeop <ssucgoo@gmail.com>
2026-01-13 21:30:56 +0900

    :sparkles: useNavigate 훅을 이용한 라우팅 코드 추가

--- frontend/one-byte-react/section11/src/App.tsx
+++ frontend/one-byte-react/section11/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
-import {Link, Route, Routes} from "react-router";
+import {Link, Route, Routes, useNavigate} from "react-router";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary.tsx";
 import Notfound from "./pages/Notfound.tsx";
 
 function App() {
+    const nav = useNavigate();
+
+    const onClickButton = () => {
+        nav("/new");
+    };
+
     return (
         <>
             <div>
@@ -13,6 +19,9 @@ function App() {
                 <Link to={"/new"}>New</Link>
                 <Link to={"/diary"}>Diary</Link>
             </div>
+            <button onClick={onClickButton}>
+                New 페이지로 이동
+            </button>
             <Routes>
             {/*Routes 컴포넌트 안에는 Route 컴포넌트만 들어갈 수 있다.*/}
                 <Route path="/" element={<Home/>}/>
