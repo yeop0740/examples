71b6b08cc30655ed98cf7370883a83626aa7d07b
heeyeop <ssucgoo@gmail.com>
2026-01-13 21:27:00 +0900

    :sparkles: link 태그 라우팅 코드 추가

--- frontend/one-byte-react/section11/src/App.tsx
+++ frontend/one-byte-react/section11/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Route, Routes} from "react-router";
+import {Link, Route, Routes} from "react-router";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary.tsx";
@@ -8,6 +8,11 @@ import Notfound from "./pages/Notfound.tsx";
 function App() {
     return (
         <>
+            <div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/new"}>New</Link>
+                <Link to={"/diary"}>Diary</Link>
+            </div>
             <Routes>
             {/*Routes 컴포넌트 안에는 Route 컴포넌트만 들어갈 수 있다.*/}
                 <Route path="/" element={<Home/>}/>
