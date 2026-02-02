813b0f885a36a8062005c86d3c8fd53ff5db86d4
heeyeop <ssucgoo@gmail.com>
2026-02-02 21:41:08 +0900

    :tada: 프로젝트 세팅

---
+++ book/practical-programming-for-spring-developers/README.md
@@ -0,0 +1,1 @@
+## Practical Programming For Spring Developers
---
+++ book/practical-programming-for-spring-developers/src/test/java/org/heeyeop/practicalprogrammingforspringdevelopers/PracticalProgrammingForSpringDevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heeyeop.practicalprogrammingforspringdevelopers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalProgrammingForSpringDevelopersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
