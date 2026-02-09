0bac72358e72e1f1550892582a6e4254b00b7ea4
heeyeop <ssucgoo@gmail.com>
2026-02-09 21:25:34 +0900

    :tada: project init

---
+++ container/k8s-client-demo-1/src/main/java/org/heeyeop/k8sclientdemo1/K8sClientDemo1Application.java
@@ -0,0 +1,13 @@
+package org.heeyeop.k8sclientdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sClientDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sClientDemo1Application.class, args);
+    }
+
+}
---
+++ container/k8s-client-demo-1/src/test/java/org/heeyeop/k8sclientdemo1/K8sClientDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heeyeop.k8sclientdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sClientDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
