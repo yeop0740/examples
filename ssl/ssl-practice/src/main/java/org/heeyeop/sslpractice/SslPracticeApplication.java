868488a852f19bd3dfd049d22f078e1ae255ed28
heeyeop <ssucgoo@gmail.com>
2026-02-10 21:41:38 +0900

    :tada: 프로젝트 세팅

---
+++ ssl/ssl-practice/src/main/java/org/heeyeop/sslpractice/SslPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.heeyeop.sslpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslPracticeApplication.class, args);
+    }
+
+}
---
+++ ssl/ssl-practice/src/test/java/org/heeyeop/sslpractice/SslPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heeyeop.sslpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
