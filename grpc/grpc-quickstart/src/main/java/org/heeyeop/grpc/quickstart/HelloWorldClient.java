0612c8eed1597c6ba8f041ed28de585997149cde
heeyeop <ssucgoo@gmail.com>
2026-04-19 14:25:55 +0900

    예제 코드 복붙

---
+++ grpc/grpc-quickstart/src/main/java/org/heeyeop/grpc/quickstart/HelloWorldClient.java
@@ -0,0 +1,61 @@
+package org.heeyeop.grpc.quickstart;
+
+import io.grpc.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorldClient {
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public HelloWorldClient(Channel channel) {
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        String user = "world2";
+        String target = "localhost:50051";
+
+        if (args.length > 0) {
+            if ("--help".equals(args[0])) {
+                System.err.println("Usage: [name [target]]");
+                System.err.println("");
+                System.err.println("  name    The name to greet");
+                System.err.println("  target  The server to connect to");
+                System.exit(1);
+            }
+            user = args[0];
+        }
+        if (args.length > 1) {
+            target = args[1];
+        }
+
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+                .build();
+        try {
+            HelloWorldClient client = new HelloWorldClient(channel);
+            client.greet(user);
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+
+}
---
+++ grpc/grpc-quickstart/src/main/java/org/heeyeop/grpc/quickstart/HelloWorldServer.java
@@ -0,0 +1,74 @@
+package org.heeyeop.grpc.quickstart;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class HelloWorldServer {
+    private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+
+    private Server server;
+
+    private void start() throws IOException {
+        int port = 50051;
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
+                .executor(executor)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            try {
+                HelloWorldServer.this.stop();
+            } catch (InterruptedException e) {
+                if (server != null) {
+                    server.shutdownNow();
+                }
+                e.printStackTrace(System.err);
+            } finally {
+                executor.shutdownNow();
+            }
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+        @Override
+        public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+
+    }
+
+}
